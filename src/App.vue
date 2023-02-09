c1321e79e6c9f07d6a1f342dd9bf4ae41f54bd2c
BSJJC <805086021@qq.com>
2023-02-09 11:06:48 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, setDevtoolsHook } from "vue"
 
-let themeNow = ref("light");
+let themeNow = ref("");
+
+// init theme
+if (localStorage.getItem("data-theme")) {
+  const settedTheme: string = localStorage.getItem("data-theme") as string;
+  document.documentElement.setAttribute("data-theme", settedTheme)
+  themeNow.value = settedTheme;
+}
+else {
+  localStorage.setItem("data-theme", "light");
+  document.documentElement.setAttribute("data-theme", "light");
+}
+
+const changTheme = (newTheme: string): void => {
+  localStorage.setItem("data-theme", newTheme);
+  document.documentElement.setAttribute("data-theme", newTheme);
+
+  themeNow.value = newTheme;
+}
+
+const switchTheme = (): void => {
+  themeNow.value === "light"
+    ? changTheme("dark")
+    : changTheme("light")
+};
 </script>
 
 <template>
   <div class="container">
-    <h1>the theme now is: {{ themeNow }} </h1>
-
-    <button>theme switch</button>
+    <h1 class="title"> the theme now is : {{ themeNow }}</h1>
+    <button @click="switchTheme">Switch Theme</button>
+    <h1 class="test">TEST</h1>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import "./scss/index.css"
+import "./styles/index.css"
 
 createApp(App).mount('#app')
