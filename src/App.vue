9c00c917c0385d3e78b70bc5b51a0013d1a45961
BSJJC <805086021@qq.com>
2023-02-09 11:09:58 +0800

    ; )

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
-import { ref, setDevtoolsHook } from "vue"
+import { ref } from "vue";
 
 let themeNow = ref("");
 
-// init theme
 if (localStorage.getItem("data-theme")) {
   const settedTheme: string = localStorage.getItem("data-theme") as string;
   document.documentElement.setAttribute("data-theme", settedTheme)
@@ -12,14 +11,14 @@ if (localStorage.getItem("data-theme")) {
 else {
   localStorage.setItem("data-theme", "light");
   document.documentElement.setAttribute("data-theme", "light");
-}
+};
 
 const changTheme = (newTheme: string): void => {
   localStorage.setItem("data-theme", newTheme);
   document.documentElement.setAttribute("data-theme", newTheme);
 
   themeNow.value = newTheme;
-}
+};
 
 const switchTheme = (): void => {
   themeNow.value === "light"
